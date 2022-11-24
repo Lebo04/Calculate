baade2bc7befe895efaf0e9f13297e8da4ef9348
Lebo04 <nobomvukelebogile497@gmail.com>
2022-11-24 16:29:24 +0200

    java

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 let sum = "";
+let operator = document.getElementById("operator").value;
 
 function clearSum() {
-  document.getElementById("output").value = " ";
+  document.getElementById("output1").value = " ";
+  document.getElementById("output2").value = " ";
 }
 
 function addToSum(value) {
-  document.getElementById("output").value += value;
+  document.getElementById("output1").value += value;
+
+  switch (operator) {
+    case "/":
+      document.getElementById("operator").disabled = true;
+      break;
+    case "-":
+      document.getElementById("operator").disabled = true;
+      break;
+    case "+":
+      document.getElementById("operator").disabled = true;
+      break;
+    case "*":
+      document.getElementById("operator").disabled = true;
+      break;
+    case ".":
+      document.getElementById("operator").disabled = true;
+      break;
+
+    default:
+      document.getElementById("operator").disabled = false;
+      break;
+  }
 }
 
 function evaluateSum() {
-  let x = document.getElementById("output").value;
-  let y = eval(x);
-  document.getElementById("output").value = y;
+  let x = document.getElementById("output1").value;
+  sum = eval(x);
+  document.getElementById("output2").value = sum;
+}
+
+function del() {
+  let view = document.getElementById("output1").value;
+  display = view.slice(0, -1);
+  document.getElementById("output1").value = display;
 }
