c32ed5bc679d86050e307c0f82bb31cdfe2403a7
Lebo04 <nobomvukelebogile497@gmail.com>
2022-11-25 16:36:24 +0200

    add

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
 let sum = "";
-let operator = document.getElementById("operator").value;
-
-function clearSum() {
-  document.getElementById("output1").value = " ";
-  document.getElementById("output2").value = " ";
-}
+let view = document.getElementById("output1").value;
 
 function addToSum(value) {
   document.getElementById("output1").value += value;
 
-  switch (operator) {
-    case "/":
-      document.getElementById("operator").disabled = true;
-      break;
-    case "-":
-      document.getElementById("operator").disabled = true;
-      break;
-    case "+":
-      document.getElementById("operator").disabled = true;
-      break;
-    case "*":
-      document.getElementById("operator").disabled = true;
-      break;
+  switch (document.getElementById("dot").value) {
     case ".":
-      document.getElementById("operator").disabled = true;
+      document.getElementById("dot").disabled = true;
       break;
-
     default:
-      document.getElementById("operator").disabled = false;
+      document.getElementById("dot").disabled = false;
       break;
+  }
+  switch (document.getElementById("times").value) {
+    case "*":
+      document.getElementById("times").disabled = true;
+      break;
+    default:
+      document.getElementById("times").disabled = false;
+      break;
+  }
+  switch (document.getElementById("slash").value) {
+    case "/":
+      document.getElementById("slash").disabled = true;
+      break;
+    default:
+      document.getElementById("slash").disabled = false;
+      break;
+  }
+  switch (document.getElementById("minus").value) {
+    case "-":
+      document.getElementById("minus").disabled = true;
+      break;
+    default:
+      document.getElementById("minus").disabled = false;
+      break;
+  }
+  switch (document.getElementById("add").value) {
+    case "+":
+      document.getElementById("add").disabled = false;
+      break;
+    default:
+      document.getElementById("add").disabled = true;
+      break;
+  }
+
+  if (view.endsWith("/")) {
+    view.replace(operator);
   }
 }
 
@@ -36,6 +54,15 @@ function evaluateSum() {
   let x = document.getElementById("output1").value;
   sum = eval(x);
   document.getElementById("output2").value = sum;
+
+  if (sum === undefined) {
+    document.getElementById("output2").value = " ";
+  }
+}
+
+function clearSum() {
+  document.getElementById("output1").value = " ";
+  document.getElementById("output2").value = " ";
 }
 
 function del() {
